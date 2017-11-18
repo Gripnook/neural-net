8d6aebd11e588b329ebf5e27ed9a0d0e35481b9d
Gripnook <XxAndreixX@hotmail.com>
2017-11-18 14:08:01 -0500

    Added playground file for experimenting

---
+++ src/playground.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from neural_network import NeuralNetwork
+from trainer import Trainer
+
+if __name__ == '__main__':
+    nn = NeuralNetwork((1, 10, 10, 1))
+
+    X = np.random.uniform(0, 2 * np.pi, 100)
+    y = 0.5 * (1 + 0.8 * np.sin(X))
+    Trainer(nn).train(X / (2 * np.pi), y, method='BFGS')
+
+    xx = 2 * np.pi * np.array(range(100)) / 100.0
+    plt.plot(xx, np.sin(xx), xx, (2 * nn.predict(xx / (2 * np.pi)) - 1) / 0.8)
+    plt.show()
