96099af755b88d6390684f3e1cd5de83519ed354
Sean Stappas <seanstappas@gmail.com>
2017-11-24 19:15:17 -0500

    Refactored out weight update from gradient descent methods.

--- src/training.py
+++ src/training.py
@@ -19,12 +19,7 @@ def stochastic_gradient_descent(nn, input_vectors, output_vectors, num_iteration
         random_output_vectors = output_vectors[random_indices]
 
         # Update the weights using the selected examples.
-        weights = nn.get_weights()
-        gradients = nn.get_loss_gradient(random_input_vectors, random_output_vectors)
-        for weight, gradient, delta_weight in zip(weights, gradients, delta_weights):
-            delta_weight[:] = -learning_rate * gradient + momentum * delta_weight
-            weight += delta_weight
-        nn.set_weights(weights)
+        update_weights(delta_weights, learning_rate, momentum, nn, random_input_vectors, random_output_vectors)
 
         # Let the user know that an iteration has completed.
         callback(iteration)
@@ -40,12 +35,16 @@ def batch_gradient_descent(nn, input_vectors, output_vectors, num_iterations=100
     delta_weights = [np.zeros(weight.shape) for weight in nn.get_weights()]
     for iteration in range(num_iterations):
         # Update the weights using the examples.
-        weights = nn.get_weights()
-        gradients = nn.get_loss_gradient(input_vectors, output_vectors)
-        for weight, gradient, delta_weight in zip(weights, gradients, delta_weights):
-            delta_weight[:] = -learning_rate * gradient + momentum * delta_weight
-            weight += delta_weight
-        nn.set_weights(weights)
+        update_weights(delta_weights, learning_rate, momentum, nn, input_vectors, output_vectors)
 
         # Let the user know that an iteration has completed.
         callback(iteration)
+
+
+def update_weights(delta_weights, learning_rate, momentum, nn, random_input_vectors, random_output_vectors):
+    weights = nn.get_weights()
+    gradients = nn.get_loss_gradient(random_input_vectors, random_output_vectors)
+    for weight, gradient, delta_weight in zip(weights, gradients, delta_weights):
+        delta_weight[:] = -learning_rate * gradient + momentum * delta_weight
+        weight += delta_weight
+    nn.set_weights(weights)
