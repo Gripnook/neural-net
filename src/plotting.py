e66444ae9c4720db9209733fb86661ee9510ea37
Sean Stappas <seanstappas@gmail.com>
2017-11-28 19:28:29 -0500

    Refactored plotting, added learning rate zoom and random predictor.

--- src/plotting.py
+++ src/plotting.py
@@ -8,18 +8,16 @@ from matplotlib.ticker import MaxNLocator
 
 from mnist_fully_connected import test_mnist_one_hot
 
-plt.rcParams["font.family"] = "Times New Roman"
-NUM_DATA_POINTS = 35
-LOGS_DIRECTORY = 'csv'
+# plt.rcParams["font.family"] = "Times New Roman"
 
 
-def plot_directory(directory, x_label, y_label):
+def plot_from_csv_in_directory(directory, x_label, y_label, num_data_points=35):
     f = plt.figure()
     ax = f.gca()
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     for filename in os.listdir(directory):
-        x_range = pd.read_csv(os.path.join(directory, filename))[x_label][1:NUM_DATA_POINTS]
-        y_range = pd.read_csv(os.path.join(directory, filename))[y_label][1:NUM_DATA_POINTS]
+        x_range = pd.read_csv(os.path.join(directory, filename))[x_label][1:num_data_points]
+        y_range = pd.read_csv(os.path.join(directory, filename))[y_label][1:num_data_points]
         splt = filename.split('.')[0].split('_')
         plt.plot(x_range, y_range, label='learning_rate={}%, layer_decay={}%'.format(splt[2], splt[4]))
     plt.xlabel(x_label)
@@ -29,33 +27,7 @@ def plot_directory(directory, x_label, y_label):
     f.savefig('plots/{}_vs_{}.pdf'.format(y_label, x_label), bbox_inches='tight')
 
 
-def plot_csv_simple(filename):
-    f = plt.figure()
-    ax = f.gca()
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    csv_file = pd.read_csv('csv/{}.csv'.format(filename))
-    x_range = csv_file['epoch']
-    y_range = csv_file['test_accuracy']
-    plt.plot(x_range, y_range)
-    plt.xlabel('Epoch')
-    plt.ylabel('Test Accuracy')
-    plt.grid(True)
-    f.savefig('plots/{}.pdf'.format(filename), bbox_inches='tight')
-
-
-def plot_test_accuracy_simple(y_range, filename):
-    f = plt.figure()
-    ax = f.gca()
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    x_range = [i for i in range(100)]
-    plt.plot(x_range, y_range)
-    plt.xlabel('Epoch')
-    plt.ylabel('Test Accuracy')
-    plt.grid(True)
-    f.savefig('plots/{}.pdf'.format(filename), bbox_inches='tight')
-
-
-def plot_test_accuracy_multiple(y_ranges, labels, filename):
+def plot_from_list(y_ranges, labels, filename):
     f = plt.figure()
     ax = f.gca()
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
@@ -69,9 +41,26 @@ def plot_test_accuracy_multiple(y_ranges, labels, filename):
     f.savefig('plots/{}.pdf'.format(filename), bbox_inches='tight')
 
 
-def plot_csv_multiple_zoom(csv_filenames, labels, filename, start_epoch=1, end_epoch=100):
+def plot_from_csv(csv_filenames, labels, filename, start_epoch=1, end_epoch=100):
     f = plt.figure()
     ax = f.gca()
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    x_range = [i for i in range(start_epoch, end_epoch)]
+    for csv_filename, label in zip(csv_filenames, labels):
+        csv_file = pd.read_csv('csv/{}.csv'.format(csv_filename))
+        y_range = csv_file['test_accuracy'][start_epoch:end_epoch]
+        plt.plot(x_range, y_range, label=label)
+    plt.xlabel('Epoch')
+    plt.ylabel('Test Accuracy')
+    plt.legend()
+    plt.grid(True)
+    f.savefig('plots/{}.pdf'.format(filename), bbox_inches='tight')
+
+
+def plot_from_csv_range(csv_filenames, labels, filename, start_epoch=0, end_epoch=100, y_min=0.9, y_max=0.98):
+    f = plt.figure()
+    ax = f.gca()
+    ax.set_ylim([y_min, y_max])
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     x_range = [i for i in range(start_epoch, end_epoch)]
     for csv_filename, label in zip(csv_filenames, labels):
@@ -95,7 +84,7 @@ def plot_network_size():
             csv_filename='network_size_{}'.format('_'.join(str(layer) for layer in hidden_layers)))
         accuracy_ranges.append(test_accuracies)
         labels.append('Hidden layers: {}'.format(hidden_layers))
-    plot_test_accuracy_multiple(accuracy_ranges, labels, 'network_size')
+    plot_from_list(accuracy_ranges, labels, 'network_size')
 
 
 def plot_network_size_csv():
@@ -104,7 +93,7 @@ def plot_network_size_csv():
                      for hidden_layers in lst_hidden_layers]
     labels = ['Hidden layer sizes: ({})'.format(', '.join(str(layer) for layer in hidden_layers))
               for hidden_layers in lst_hidden_layers]
-    plot_csv_multiple_zoom(csv_filenames, labels, 'network_size', start_epoch=0)
+    plot_from_csv(csv_filenames, labels, 'network_size', start_epoch=0)
 
 
 def plot_network_size_csv_zoom():
@@ -113,14 +102,13 @@ def plot_network_size_csv_zoom():
                      for hidden_layers in lst_hidden_layers]
     labels = ['Hidden layer sizes: ({})'.format(','.join(str(layer) for layer in hidden_layers))
               for hidden_layers in lst_hidden_layers]
-    plot_csv_multiple_zoom(csv_filenames, labels, 'network_size_zoom')
+    plot_from_csv(csv_filenames, labels, 'network_size_zoom')
 
 
 def plot_logistic_vs_tanh():
     test_accuracies_logistic = test_mnist_one_hot(sigmoid='logistic', learning_rate=0.1178, csv_filename='logistic')
     test_accuracies_tanh = test_mnist_one_hot(sigmoid='tanh', csv_filename='tanh')
-    plot_test_accuracy_multiple((test_accuracies_logistic, test_accuracies_tanh), ('logistic', 'tanh'),
-                                'logistic_vs_tanh')
+    plot_from_list((test_accuracies_logistic, test_accuracies_tanh), ('logistic', 'tanh'), 'logistic_vs_tanh')
 
 
 def plot_learning_rate():
@@ -132,14 +120,21 @@ def plot_learning_rate():
                                              csv_filename='learning_rate_{}'.format(int(learning_rate * 100)))
         accuracy_ranges.append(test_accuracies)
         labels.append('Learning rate: {}'.format(learning_rate))
-    plot_test_accuracy_multiple(accuracy_ranges, labels, 'learning_rate')
+    plot_from_list(accuracy_ranges, labels, 'learning_rate')
 
 
 def plot_learning_rate_csv_zoom():
     learning_rates = [0.01, 0.02, 0.05, 0.1]
     csv_filenames = ['learning_rate_{}'.format(int(learning_rate * 100)) for learning_rate in learning_rates]
     labels = ['Learning rate: {}'.format(learning_rate) for learning_rate in learning_rates]
-    plot_csv_multiple_zoom(csv_filenames, labels, 'learning_rate_zoom')
+    plot_from_csv(csv_filenames, labels, 'learning_rate_zoom')
+
+
+def plot_learning_rate_csv_zoom_2():
+    learning_rates = [0.01, 0.02, 0.05, 0.1]
+    csv_filenames = ['learning_rate_{}'.format(int(learning_rate * 100)) for learning_rate in learning_rates]
+    labels = ['Learning rate: {}'.format(learning_rate) for learning_rate in learning_rates]
+    plot_from_csv_range(csv_filenames, labels, 'learning_rate_zoom_2')
 
 
 def plot_batch_size():
@@ -150,14 +145,14 @@ def plot_batch_size():
         test_accuracies = test_mnist_one_hot(batch_size=batch_size, csv_filename='batch_size_{}'.format(batch_size))
         accuracy_ranges.append(test_accuracies)
         labels.append('Batch size: {}'.format(batch_size))
-    plot_test_accuracy_multiple(accuracy_ranges, labels, 'batch_size')
+    plot_from_list(accuracy_ranges, labels, 'batch_size')
 
 
 def plot_batch_size_csv_zoom():
     batch_sizes = [1, 10, 100]
     csv_filenames = ['batch_size_{}'.format(batch_size) for batch_size in batch_sizes]
     labels = ['Batch size: {}'.format(batch_size) for batch_size in batch_sizes]
-    plot_csv_multiple_zoom(csv_filenames, labels, 'batch_size_zoom')
+    plot_from_csv(csv_filenames, labels, 'batch_size_zoom')
 
 
 def plot_momentum():
@@ -168,14 +163,14 @@ def plot_momentum():
         test_accuracies = test_mnist_one_hot(momentum=momentum, csv_filename='momentum_{}'.format(int(momentum * 100)))
         accuracy_ranges.append(test_accuracies)
         labels.append('Momentum: {}'.format(momentum))
-    plot_test_accuracy_multiple(accuracy_ranges, labels, 'momentum')
+    plot_from_list(accuracy_ranges, labels, 'momentum')
 
 
 def plot_momentum_csv_zoom():
     momenta = [0.0, 0.3, 0.6, 0.9]
     csv_filenames = ['momentum_{}'.format(int(momentum * 100)) for momentum in momenta]
     labels = ['Momentum: {}'.format(momentum) for momentum in momenta]
-    plot_csv_multiple_zoom(csv_filenames, labels, 'momentum_zoom')
+    plot_from_csv(csv_filenames, labels, 'momentum_zoom')
 
 
 def plot_layer_decay():
@@ -188,7 +183,7 @@ def plot_layer_decay():
             csv_filename='layer_decay_{}'.format(int(layer_decay * 100)))
         accuracy_ranges.append(test_accuracies)
         labels.append('Layer decay: {}'.format(layer_decay))
-    plot_test_accuracy_multiple(accuracy_ranges, labels, 'layer_decay')
+    plot_from_list(accuracy_ranges, labels, 'layer_decay')
 
 
 def plot_layer_decay_csv_zoom():
@@ -196,13 +191,21 @@ def plot_layer_decay_csv_zoom():
     csv_filenames = ['layer_decay_{}'.format(int(layer_decay * 100)) for layer_decay in
                      layer_decays]
     labels = ['Layer decay: {}'.format(layer_decay) for layer_decay in layer_decays]
-    plot_csv_multiple_zoom(csv_filenames, labels, 'layer_decay_zoom')
+    plot_from_csv(csv_filenames, labels, 'layer_decay_zoom')
+
+
+def plot_network_comparison():
+    # TODO: ConvNet filename & label
+    csv_filenames = ('random', 'network_size_300', '')
+    labels = ('Random predictor', 'Fully connected network with 300 hidden units', 'Convolutional network')
+    plot_from_csv(csv_filenames, labels, 'network_comparison')
 
 
 if __name__ == '__main__':
     plot_logistic_vs_tanh()
     plot_learning_rate()
     plot_learning_rate_csv_zoom()
+    plot_learning_rate_csv_zoom_2()
     plot_batch_size()
     plot_batch_size_csv_zoom()
     plot_momentum()
@@ -211,3 +214,4 @@ if __name__ == '__main__':
     plot_layer_decay_csv_zoom()
     plot_network_size()
     plot_network_size_csv_zoom()
+    plot_network_comparison()
